00020a61d9a445041518c9d6fdcf6967aeee6a88
Kipp Jennings <jenningskipp@gmail.com>
2023-11-20 20:03:38 -0600

    movie schedule demo
    a small program to show how dictionaries work

---
+++ dictionaries,json,pip/movie_schedule.py
@@ -0,0 +1,18 @@
+
+current_movies = {'Pulp Fiction': '12:00pm',
+                  'Curious George': '12:45pm',
+                  'Home Alone 6: How Does This Keep Happening?' :'4:35pm'}
+
+print("We're showing the follwing movies:")
+for key in current_movies:
+    print(key)
+
+
+movie = input('What movie would you like the showtime for?\n')
+
+showtime = current_movies.get(movie)
+
+if showtime == None:
+    print("Requested movie isn't playing")
+else:
+    print(movie, 'is playing at', showtime)
